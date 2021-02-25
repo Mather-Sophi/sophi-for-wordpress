7c94996bfce43714d190f06a2c61bc7c1d5bfd46
Tung Du <dinhtungdu@gmail.com>
2021-02-25 16:51:34 +0700

    feat: enqueue js file

--- includes/functions/tracking.php
+++ includes/functions/tracking.php
@@ -19,52 +19,75 @@ function setup() {
 		return __NAMESPACE__ . "\\$function";
 	};
 
-	if ( is_archive() || is_singular() ) {
-		add_action( 'wp_enqueue_scripts', $n( 'enqueue_scripts' ) );
-	}
+	add_action( 'wp_enqueue_scripts', $n( 'enqueue_scripts' ) );
 }
 
 /**
  * Enqueue tracking scripts.
  */
 function enqueue_scripts() {
-}
+	if ( ! is_archive() && ! is_singular() ) {
+		return;
+	}
 
-/**
- * Prepare data for JS tracking on single page.
- */
-function get_single_post_tracking_data() {
-	$post = get_post();
-	return apply_filters(
-		'sophi_post_tracking_data',
-		[
-			'pageType' => 'article',
-			'breadcrumbs' => get_breadcrumbs( $post ),
-			'sectionName' => get_section_name( get_breadcrumbs( $post ) ),
-			'environment' => get_sophi_settings( 'environment' ),
-			'environmentVersion' => get_bloginfo( 'version' ),
-			'contentType' => 'article',
-			'contentId' => $post->ID,
-		]
+	wp_enqueue_script( 'sophi-tag', SOPHI_WP_URL . '/dist/js/sophi-tag.js', [], SOPHI_WP_VERSION );
+	wp_localize_script(
+		'sophi-tag',
+		'SOPHIDATA',
+		get_sophi_tracking_data()
 	);
 }
 
 /**
- * Prepare data for JS tracking on archive page.
+ * Prepare data for JS tracking.
  */
-function get_archive_tracking_data() {
-	global $wp;
-	return apply_filters(
-		'sophi_archive_tracking_data',
-		[
-			'pageType' => 'section',
-			'breadcrumbs' => $wp->request,
-			'sectionName' => get_section_name( $wp->request ),
-			'environment' => get_sophi_settings( 'environment' ),
-			'environmentVersion' => get_bloginfo( 'version' ),
-			'contentType' => 'article',
-		]
-	);
+function get_sophi_tracking_data() {
+	global $wp, $post;
+
+	$urlparts = parse_url( home_url() );
+	$domain   = $urlparts['host'];
+
+	$data = [
+		'data'     => [
+			'environment' => [
+				'environment' => get_sophi_settings( 'environment' ),
+				'version'     => get_bloginfo( 'version' ),
+			],
+			'page'        => [
+				'type'       => 'article',
+				'breadcrumb' => is_singular() ? get_breadcrumbs( $post ) : $wp->request,
+			],
+			'content'     => [
+				'type' => 'article',
+			],
+		],
+		'settings' => [
+			'client'        => $domain,
+			'appId'         => $domain,
+			'linkedDomains' => [ $domain ],
+			'plugin'        => [
+				'adblock' => false,
+				'private' => false,
+				'video'   => false,
+			],
+		],
+
+	];
+
+	$section = is_singular() ? get_section_name( get_breadcrumbs( $post ) ) : get_section_name( $wp->request );
+
+	if ( $section ) {
+		$data['data']['page']['sectionName'] = $section;
+	}
+
+	if ( is_singular() ) {
+		$data['data']['content']['contentId'] = $post->ID;
+	}
+
+	if ( is_singular() ) {
+		return apply_filters( 'sophi_post_tracking_data', $data );
+	}
+	return apply_filters( 'sophi_archive_tracking_data', $data );
 }
 
 /**
@@ -91,10 +114,13 @@ function get_breadcrumbs( $post ) {
  */
 function get_section_name( $path = '' ) {
 	$parts = explode( '/', $path );
+	$parts = array_filter( $parts );
 
-	if ( 1 === count( $parts ) ) {
+	if ( 2 !== count( $parts ) ) {
 		return '';
 	}
 
-	return array_slice( $parts, -2, 1 );
+	$section = array_slice( $parts, -2, 1 );
+
+	return reset( $section );
 }
