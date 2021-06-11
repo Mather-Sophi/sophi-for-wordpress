22b25f616a459eb19efef6ad6922121df6cb998a
Tung Du <dinhtungdu@gmail.com>
2021-06-11 08:01:47 +0700

    remove sectionName

--- includes/functions/content-sync.php
+++ includes/functions/content-sync.php
@@ -147,7 +147,6 @@ function get_post_data( $post ) {
 		'publishedAt'    => gmdate( \DateTime::RFC3339, strtotime( $post->post_date_gmt ) ),
 		'plainText'      => wp_strip_all_tags( $content ),
 		'contentSize'    => str_word_count( wp_strip_all_tags( $content ) ),
-		'sectionName'    => Utils\get_section_name( Utils\get_breadcrumb( $post ) ),
 		'sectionNames'   => Utils\get_section_names( Utils\get_breadcrumb( $post ) ),
 		'modifiedAt'     => gmdate( \DateTime::RFC3339, strtotime( $post->post_modified_gmt ) ),
 		'tags'           => Utils\get_post_tags( $post ),
