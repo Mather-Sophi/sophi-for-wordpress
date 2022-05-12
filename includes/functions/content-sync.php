8f13bd00665830e72396edb423bba2bc9cea4317
Max Lyuchin <maxim.lyuchin@gmail.com>
2022-05-12 16:57:38 +0300

    Suppress output of trackUnstruckEvent

--- includes/functions/content-sync.php
+++ includes/functions/content-sync.php
@@ -145,6 +145,15 @@ function send_track_event( $tracker, $post, $action ) {
 	 * @return {array} Tracking data to send.
 	 */
 	$data = apply_filters_ref_array( 'sophi_cms_tracking_request_data', array( $data, &$tracker, $post, $action ) );
+
+	/** This filter is documented in includes/classes/SiteAutomation/Request.php */
+	$debug = apply_filters( 'sophi_tracker_emitter_debug', false );
+
+	// Suppress stdout from Emitters in debug mode.
+	if ( true === $debug ) {
+		ob_start();
+	}
+
 	$tracker->trackUnstructEvent(
 		[
 			'schema' => 'iglu:com.sophi/content_update/jsonschema/2-0-3',
@@ -160,6 +169,10 @@ function send_track_event( $tracker, $post, $action ) {
 			],
 		]
 	);
+
+	if ( true === $debug ) {
+		ob_end_clean();
+	}
 
 	/**
 	 * Fires after tracker sends the request.
