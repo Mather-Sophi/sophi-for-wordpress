a7a72cd3076fbf3019f6a7117d051268653fc823
faisal-alvi <faisalalvi33@gmail.com>
2022-10-03 20:21:37 +0530

    saving the innerBlocks

--- includes/blocks/site-automation-block/save.js
+++ includes/blocks/site-automation-block/save.js
@@ -1,8 +1,18 @@
+import { useBlockProps, InnerBlocks } from '@wordpress/block-editor';
+
 /**
  * See https://wordpress.org/gutenberg/handbook/designers-developers/developers/block-api/block-edit-save/#save
  *
  * @return {null} Dynamic blocks do not save the HTML.
  */
-const SiteAutomationBlockSave = () => null;
+const SiteAutomationBlockSave = () => {
+	return (
+		<div { ...useBlockProps.save() }>
+			<div>
+				<InnerBlocks.Content />
+			</div>
+		</div>
+	)
+};
 
 export default SiteAutomationBlockSave;
