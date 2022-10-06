b946d39edfcb922c665bca43d4b43a9ce65558c6
Max Lyuchin <maxim.lyuchin@gmail.com>
2022-10-06 12:42:08 +0300

    Unit tests for maybe_skip_track_event()

---
+++ tests/phpunit/test-tools/SkipTracking_Tests.php
@@ -0,0 +1,87 @@
+<?php
+namespace SophiWP\ContentSync;
+
+use SophiWP as Base;
+
+class SkipTracking_Tests extends Base\TestCase {
+
+	protected $testFiles = array(
+		'functions/content-sync.php',
+	);
+
+	public function setUp() :void {
+		\WP_Mock::setUp();
+
+		\WP_Mock::userFunction( 'wp_hash' )->andReturnUsing(
+			function( $data ) {
+				return md5( $data );
+			}
+		);
+
+		\WP_Mock::userFunction( 'wp_json_encode' )->andReturnUsing(
+			function( $data ) {
+				return json_encode( $data );
+			}
+		);
+
+		parent::setUp();
+	}
+
+	public function tearDown() :void {
+		\WP_Mock::tearDown();
+	}
+
+	/**
+	 * Test Maybe Skip logic
+	 *
+	 * @covers \SophiWP\ContentSync\maybe_skip_track_event
+	 * @dataProvider data_provider_for_test_maybe_skip
+	 */
+	public function test_maybe_skip( $data, $get_transient, $set_transient, $should_skip ) {
+
+		\WP_Mock::userFunction( 'wp_hash' )->andReturn( '' );
+		\WP_Mock::userFunction( 'wp_json_encode' )->andReturn( '' );
+		\WP_Mock::userFunction( 'get_transient' )->andReturn( $get_transient );
+
+		if ( $set_transient ) {
+			\WP_Mock::userFunction( 'set_transient' )->with( 'sophi_tracking_request_', $set_transient, 10 );
+		}
+
+		$this->assertSame( $should_skip, maybe_skip_track_event( $data ) );
+	}
+
+	public function data_provider_for_test_maybe_skip() {
+		return array(
+			'Should ignore modifiedAt argument'         => array(
+				'data'          => array( 'a' => 'b', 'modifiedAt' => 'c' ),
+				'get_transient' => false,
+				'set_transient' => array( 'a' => 'b' ),
+				'should_skip'   => false,
+			),
+			'Should skip when transient exists'         => array(
+				'data'          => array( 'a' => 'b' ),
+				'get_transient' => array( 'a' => 'b' ),
+				'set_transient' => false,
+				'should_skip'   => true,
+			),
+			'Should not skip when transient is not set' => array(
+				'data'          => array( 'a' => 'b' ),
+				'get_transient' => false,
+				'set_transient' => array( 'a' => 'b' ),
+				'should_skip'   => false,
+			),
+			'Should not skip when previous request was different' => array(
+				'data'          => array( 'a' => 'b' ),
+				'get_transient' => array( 'c' => 'd' ),
+				'set_transient' => array( 'a' => 'b' ),
+				'should_skip'   => false,
+			),
+		);
+	}
+	/**
+	 * Test get number of embedded images
+	 */
+	public function test_should_pass_untracked_event() {
+		$this->assertTrue( true );
+	}
+}
