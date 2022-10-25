e911f6a87029c6389d38d1607e8db771558ecda1
faisal-alvi <faisalalvi33@gmail.com>
2022-10-25 14:04:29 +0530

    en/decryption methods added and used

--- includes/classes/SiteAutomation/Auth.php
+++ includes/classes/SiteAutomation/Auth.php
@@ -13,11 +13,114 @@ use function SophiWP\Settings\get_sophi_settings;
  * Class: Auth
  */
 class Auth {
+
+	/**
+	 * Encryption Key.
+	 */
+	private $key;
+
+	/**
+	 * Encryption Salt.
+	 */
+	private $salt;
+
 	/**
 	 * Class constructor.
 	 */
 	public function __construct() {
 		$this->environment = get_sophi_settings( 'environment' );
+
+		// Get the required values.
+		$this->key  = $this->get_default_key();
+		$this->salt = $this->get_default_salt();
+	}
+
+	/**
+	 * Get the Logged-in key for encryption.
+	 * 
+	 * @since 1.3.0
+	 * 
+	 * @return string Logged-in key or a predefined one.
+	 */
+	private function get_default_key() {
+		if ( '' !== wp_salt( 'logged_in' ) ) {
+			return wp_salt( 'logged_in' ) ;
+		}
+
+		// If this is reached, you're either not on a live site or have a serious security issue.
+		return 'das-ist-kein-geheimer-schluessel';
+	}
+
+	/**
+	 * Get the SALT key for encryption.
+	 * 
+	 * @since 1.3.0
+	 * 
+	 * @return string SALT key or a predefined one.
+	 */
+	private function get_default_salt() {
+		if ( '' !== LOGGED_IN_SALT ) {
+			return LOGGED_IN_SALT;
+		}
+
+		// If this is reached, you're either not on a live site or have a serious security issue.
+		return 'das-ist-kein-geheimes-salz';
+	}
+
+	/**
+	 * The encryption method.
+	 * 
+	 * @since 1.3.0
+	 * 
+	 * @param string $value the value to be encrypted.
+	 * 
+	 * @return string the encrypted value OR false on failure.
+	 */
+	public function encrypt( $value ) {
+		if ( ! extension_loaded( 'openssl' ) ) {
+			return $value;
+		}
+
+		$method = 'aes-256-ctr';
+		$ivlen  = openssl_cipher_iv_length( $method );
+		$iv     = openssl_random_pseudo_bytes( $ivlen );
+
+		$raw_value = openssl_encrypt( $value . $this->salt, $method, $this->key, 0, $iv );
+		if ( ! $raw_value ) {
+			return false;
+		}
+
+		return base64_encode( $iv . $raw_value ); // phpcs:ignore WordPress.PHP.DiscouragedPHPFunctions.obfuscation_base64_encode
+	}
+
+	/**
+	 * The decryption method.
+	 * 
+	 * @since 1.3.0
+	 * 
+	 * @param string $value the value to be decrypted.
+	 * 
+	 * @return string the decrypted value OR false on failure.
+	 */
+	public function decrypt( $raw_value ) {
+		if ( ! extension_loaded( 'openssl' ) ) {
+			return $raw_value;
+		}
+
+		$raw_value = base64_decode( $raw_value, true ); // phpcs:ignore WordPress.PHP.DiscouragedPHPFunctions.obfuscation_base64_decode
+
+		$method = 'aes-256-ctr';
+		$ivlen  = openssl_cipher_iv_length( $method );
+		$iv     = substr( $raw_value, 0, $ivlen );
+
+		$raw_value = substr( $raw_value, $ivlen );
+
+		$value = openssl_decrypt( $raw_value, $method, $this->key, 0, $iv );
+		if ( ! $value || substr( $value, - strlen( $this->salt ) ) !== $this->salt ) {
+			return false;
+		}
+
+		return substr( $value, 0, - strlen( $this->salt ) );
 	}
 
 	/**
@@ -26,7 +129,7 @@ class Auth {
 	 * @return string|\WP_Error
 	 */
 	public function get_access_token() {
-		$access_token = get_transient( 'sophi_site_automation_access_token' );
+		$access_token = $this->decrypt( get_transient( 'sophi_site_automation_access_token' ) );
 
 		if ( $access_token ) {
 			return $access_token;
@@ -47,7 +150,7 @@ class Auth {
 			return $response;
 		}
 
-		set_transient( 'sophi_site_automation_access_token', $response['access_token'], $response['expires_in'] );
+		set_transient( 'sophi_site_automation_access_token', $this->encrypt( $response['access_token'] ), $response['expires_in'] );
 
 		return $response['access_token'];
 	}
