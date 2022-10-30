01e0adb3fb943c527843108fc58538f584cb2e8b
Siddharth Thevaril <siddharth.thevaril@gmail.com>
2022-10-30 20:35:54 +0530

    add PHPUnit tests

--- tests/phpunit/test-tools/Utils_Tests.php
+++ tests/phpunit/test-tools/Utils_Tests.php
@@ -45,4 +45,88 @@ class Core_Tests extends Base\TestCase {
 			),
 		);
 	}
+
+	public function test_get_primary_category__default_category__yoast_deactivated() {
+		$term_array = array(
+			(object) array(
+				'term_id' => 10,
+				'name'    => 'Uncategorized',
+			)
+		);
+
+		\WP_Mock::userFunction( 'wp_get_post_terms', array(
+			'times'  => 1,
+			'args'   => array( null, 'category' ),
+			'return' => $term_array,
+		) );
+
+		$term_name = get_primary_category();
+
+		$this->assertEquals( $term_name, 'Uncategorized' );
+	}
+
+	public function test_get_primary_category__multiple_categories__yoast_deactivated() {
+		$term_array = array(
+			(object) array(
+				'term_id' => 10,
+				'name'    => 'Science',
+			),
+			(object) array(
+				'term_id' => 10,
+				'name'    => 'Uncategorized',
+			)
+		);
+
+		\WP_Mock::userFunction( 'wp_get_post_terms', array(
+			'times'  => 1,
+			'args'   => array( null, 'category' ),
+			'return' => $term_array,
+		) );
+
+		$term_name = get_primary_category();
+
+		$this->assertEquals( $term_name, 'Science' );
+	}
+
+	public function test_get_primary_category__default_category__yoast_activated() {
+		\WP_Mock::userFunction( 'yoast_get_primary_term_id', array(
+			'times'  => 1,
+			'args'   => array( 'category', null ),
+			'return' => 123,
+		) );
+
+		\WP_Mock::userFunction( 'get_term', array(
+			'times'  => 1,
+			'args'   => array( 123 ),
+			'return' => (object) array(
+				'term_id' => 123,
+				'name'    => 'Uncategorized',
+			),
+		) );
+
+		$term_name = get_primary_category();
+
+		$this->assertEquals( $term_name, 'Uncategorized' );
+	}
+
+	public function test_get_primary_category__custom_category__yoast_activated() {
+		\WP_Mock::userFunction( 'yoast_get_primary_term_id', array(
+			'times'  => 1,
+			'args'   => array( 'category', null ),
+			'return' => 123,
+		) );
+
+		\WP_Mock::userFunction( 'get_term', array(
+			'times'  => 1,
+			'args'   => array( 123 ),
+			'return' => (object) array(
+				'term_id' => 123,
+				'name'    => 'Art',
+			),
+		) );
+
+		$term_name = get_primary_category();
+
+		$this->assertEquals( $term_name, 'Art' );
+	}
 }
